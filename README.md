a3fb3b26e6d954edfae38fb2ded2110adae420f2
Martin Reinhardt <contact@martinreinhardt-online.de>
2025-08-01 10:26:00 +0200

    chore: Finish coverage info

---
+++ README.md
@@ -0,0 +1,3 @@
+# XBau Releases
+
+[![codecov](https://codecov.io/gh/t12v/xbau-releases/graph/badge.svg?token=OSCMY1GAZJ)](https://codecov.io/gh/t12v/xbau-releases)
--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-    roots: [
-      //'src',
-      'lib',
-      'test',
-    ],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverage: true,
-    collectCoverageFrom: [
-      'index.ts',
-      'lib/**',
-    ],
-    setupFilesAfterEnv: [
-      'jest-extended/all',
-    ],
-    transformIgnorePatterns: [
-        'node_modules/(?!(string-width|strip-ansi|ansi-regex|test-json-import)/)',
-    ],
-    'transform': {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-  };
+  roots: [
+    //'src',
+    'lib',
+    'test',
+  ],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['index.ts', 'lib/**'],
+  setupFilesAfterEnv: ['jest-extended/all'],
+  transformIgnorePatterns: ['node_modules/(?!(string-width|strip-ansi|ansi-regex|test-json-import)/)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+};
