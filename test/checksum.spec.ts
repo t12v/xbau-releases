76cadb65297dc8372f9977247e85c23d25d7302e
Martin Reinhardt <contact@martinreinhardt-online.de>
2025-08-07 08:48:21 +0200

    feat(Update): Refactor update method and use sub directories
    see #OSS-35

--- test/checksum.spec.ts
+++ test/checksum.spec.ts
@@ -1,18 +1,17 @@
 /// <reference types="jest-extended" />
 import { getChecksum } from '../lib/checksum';
-import { Standard } from '../lib/types';
 
 const SECONDS = 10;
 jest.setTimeout(1000 * SECONDS);
 
-test('checksum different for different standards', async () => {
-  const checksumXbau = await getChecksum(Standard.XBAU);
-  const checksumXbauKernmodul = await getChecksum(Standard.XBAU_KERN);
+test('checksum different for different objects', async () => {
+  const checksumXbau = await getChecksum({ name: 'XBAU' });
+  const checksumXbauKernmodul = await getChecksum({ name: 'XBAU2' });
   expect(checksumXbau !== checksumXbauKernmodul).toBeTrue();
 });
 
-test('checksum same for same standards', async () => {
-  const checksumXbau = await getChecksum(Standard.XBAU);
-  const checksumXbauKernmodul = await getChecksum(Standard.XBAU);
+test('checksum same for same objects', async () => {
+  const checksumXbau = await getChecksum({ name: 'XBAU' });
+  const checksumXbauKernmodul = await getChecksum({ name: 'XBAU' });
   expect(checksumXbau === checksumXbauKernmodul).toBeTrue();
 });
