d226570f15bf05353f4faf79a0f4ee952959cfa4
Martin Reinhardt <contact@martinreinhardt-online.de>
2025-09-16 09:06:32 +0200

    chore: Increase async timeouts

--- test/update.spec.ts
+++ test/update.spec.ts
@@ -2,8 +2,8 @@
 import { Standard } from '../lib/types';
 import { update } from '../lib/update';
 
-const SECONDS = 10;
-jest.setTimeout(1000 * SECONDS);
+const MINUTES = 1;
+jest.setTimeout(1000 * MINUTES * 60);
 
 test('check for updates', async () => {
   expect(await update(Standard.XBAU, false)).toBeDefined();
